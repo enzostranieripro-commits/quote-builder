e9e827a2d8ee92d2240ff2466a79a759c7a1e426
enzostranieripro-commits <enzostranieri.pro@gmail.com>
2026-03-17 00:02:39 +0100

    Update devis.ts

--- src/types/devis.ts
+++ src/types/devis.ts
@@ -1,1 +1,13 @@
+export type {
+  DevisStatus, Unite, TvaRate, LigneDevis, Client,
+  EcheanceItem, Devis, Entreprise, SecteurId,
+} from "./index";
 
+export {
+  UNITES, TVA_RATES, STATUS_LABELS, STATUS_COLORS,
+  calcHT as calculerTotalLigneHT,
+  calcTotaux as calculerTotauxDevis,
+  fmt as formatMontant,
+  creerDevisVide, genNumeroDevis,
+  CATEGORIES_PAR_SECTEUR,
+} from "./index";
